b2cc7465f04202dd7426c9bee1f6502ce4d75010
Justin Bermudez <justinanbermudez@gmail.com>
2019-12-12 12:44:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,5 +20,5 @@ function whileLoop(number) {
 function doWhileLoop(integer) {
   do{
     console.log('I run once regardless.');
-  }while(incrementVariable() < integer)
+  }while(integer > incrementVariable())
 }
